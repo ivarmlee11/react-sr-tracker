3c15428904765c149f13b06a8d893a14881d3fbf
ivarmlee11 <ivarr5@gmail.com>
2019-07-01 03:44:02 -0700

    Context provider

--- src/components/display.js
+++ src/components/display.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import { FirebaseContext } from './firebase/';
 
 class Display extends React.Component {
     constructor(props) {
         super(props);
     }
-
+  
     render() {
         return (
         <div className="display">
-            <label htmlFor="wins">
-                Wins
-            </label>
-            <textarea
-                id="wins"
-            />
+            <FirebaseContext.Consumer>
+                {firebase => {
+                    return (
+                        <div>
+                            <label htmlFor="wins">
+                                Wins
+                            </label>
+                            <textarea
+                                id="wins"
+                            />
+                        </div>
+                    );
+                }}
+            </FirebaseContext.Consumer>
 
             <label htmlFor="losses">
                 Losses
